12d6715bb24e2acb766106d160d6c75bd5b42278
GJDeY <gregoryjdeyoung@u.northwestern.edu>
2018-09-18 17:43:30 -0500

    final restart db built

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Plain Hamburger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Avocado Cali Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('The "Beast" Burger', false);
---
+++ server.js
@@ -0,0 +1,7 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+
+var app = express();
+
+// Set port or use an available post with code below
+var PORT = process.env.PORT || 8080;
